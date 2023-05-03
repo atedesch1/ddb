4ccf992ec6fc9bb2b9a67f4b47ce08b70c69adb0
atedesch1 <alvarotedeschi@gmail.com>
2023-05-03 14:40:49 -0300

    implement simple server binary

--- src/log/server/main.rs
+++ src/log/server/main.rs
@@ -1,7 +1,15 @@
+use ddb::log::{service::LogService, rpc::log_server::LogServer};
+use tonic::transport::Server;
 
 #[tokio::main]
 async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let service = LogService::new()?;
 
+    Server::builder()
+        .add_service(LogServer::new(service))
+        .serve(addr)
+        .await?;
 
     return Ok(());
 }
