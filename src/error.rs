c1bdb8bf3e8be4bda2c52165de5795fb977a1bdd
atedesch1 <alvarotedeschi@gmail.com>
2023-05-08 16:05:53 -0300

    add more From errors

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,7 @@
-use std::fmt::{self, Display};
+use std::{
+    fmt::{self, Display},
+    net::AddrParseError,
+};
 
 /// Result returning Error
 pub type Result<T> = std::result::Result<T, Error>;
@@ -31,6 +34,12 @@ impl From<std::io::Error> for Error {
     }
 }
 
+impl From<AddrParseError> for Error {
+    fn from(err: AddrParseError) -> Self {
+        Error::Internal(err.to_string())
+    }
+}
+
 impl<T> From<std::sync::TryLockError<T>> for Error {
     fn from(err: std::sync::TryLockError<T>) -> Self {
         Error::Internal(err.to_string())
@@ -43,8 +52,44 @@ impl<T> From<std::sync::PoisonError<T>> for Error {
     }
 }
 
+impl From<tokio::task::JoinError> for Error {
+    fn from(err: tokio::task::JoinError) -> Self {
+        Error::Internal(err.to_string())
+    }
+}
+
+impl<T> From<tokio::sync::mpsc::error::SendError<T>> for Error {
+    fn from(err: tokio::sync::mpsc::error::SendError<T>) -> Self {
+        Error::Internal(err.to_string())
+    }
+}
+
+impl<T> From<tokio::sync::mpsc::error::TrySendError<T>> for Error {
+    fn from(err: tokio::sync::mpsc::error::TrySendError<T>) -> Self {
+        Error::Internal(err.to_string())
+    }
+}
+
+impl From<tokio::sync::oneshot::error::RecvError> for Error {
+    fn from(err: tokio::sync::oneshot::error::RecvError) -> Self {
+        Error::Internal(err.to_string())
+    }
+}
+
 impl From<Error> for tonic::Status {
     fn from(err: Error) -> Self {
         tonic::Status::internal(err.to_string())
+    }
+}
+
+impl From<tonic::Status> for Error {
+    fn from(err: tonic::Status) -> Self {
+        Error::Internal(err.to_string())
+    }
+}
+
+impl From<tonic::transport::Error> for Error {
+    fn from(err: tonic::transport::Error) -> Self {
+        Error::Internal(err.to_string())
     }
 }
